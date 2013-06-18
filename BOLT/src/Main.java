5d1f07acee30c613b12ab87d6dde48a36f9ace48
Ichmed <mijomeyer@t-online.de>
2013-06-18 21:33:50 +0200

    Main Class

--- BOLT/src/Main.java
+++ BOLT/src/Main.java
@@ -1,4 +1,36 @@
+import static org.lwjgl.util.glu.GLU.gluPerspective;
+import static org.lwjgl.opengl.GL11.*;
+
+import org.lwjgl.LWJGLException;
+import org.lwjgl.opengl.Display;
+
 public class Main 
 {
-
+	public static void main(String[] args)
+	{
+		try
+		{
+			Display.setDisplayMode(Display.getDesktopDisplayMode());
+			Display.create();
+		}
+		catch (LWJGLException e)
+		{
+			e.printStackTrace();
+		}
+		
+		glMatrixMode(GL_PROJECTION);
+		glLoadIdentity();
+		gluPerspective(140, (float)Display.getWidth() / Display.getHeight(), 0.001f, 100);
+		glMatrixMode(GL_MODELVIEW);
+		
+		while(!Display.isCloseRequested()) gameLoop();
+		
+		Display.destroy();
+	}
+	
+	public static void gameLoop()
+	{
+		Display.update();
+		Display.sync(50);
+	}
 }

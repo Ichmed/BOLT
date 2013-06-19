854c92eb435b70b34e9de2b24f82c838c19330aa
Ichmed <mijomeyer@t-online.de>
2013-06-19 15:20:59 +0200

    Textured Vertexes

--- BOLT/src/render/Model.java
+++ BOLT/src/render/Model.java
@@ -13,6 +13,9 @@ public class Model
 	public List<Vector3f> vertices = new ArrayList<Vector3f>();
 	public List<Vector3f> normals = new ArrayList<Vector3f>();
 	public List<Vector2f> tetxures = new ArrayList<Vector2f>();
+	
+	public boolean hasNormals = false;
+	public boolean hasTextures = false;
 
 	public List<Face> faces = new ArrayList<Face>();
 
@@ -25,24 +28,43 @@ public class Model
 		glBegin(GL_TRIANGLES);
 		for (Face face : this.faces)
 		{
-			Vector3f n1 = this.normals.get((int) face.normal.x - 1);
-			glNormal3f(n1.x, n1.y, n1.z);
-			Vector2f t1 = this.tetxures.get((int)face.texture.x - 1);
-			glTexCoord2f(t1.x, t1.y);
+			if(this.hasNormals)
+			{
+				Vector3f n1 = this.normals.get((int) face.normal.x - 1);
+				glNormal3f(n1.x, n1.y, n1.z);
+			}
+			if(this.hasTextures)
+			{
+				Vector2f t1 = this.tetxures.get((int)face.texture.x - 1);
+				glTexCoord2f(t1.x, t1.y);
+			}
 			Vector3f v1 = this.vertices.get((int) face.vertex.x - 1);
 			glVertex3f(v1.x, v1.y, v1.z);
 
-			Vector3f n2 = this.normals.get((int) face.normal.y - 1);
-			glNormal3f(n2.x, n2.y, n2.z);
-			Vector2f t2 = this.tetxures.get((int)face.texture.y - 1);
-			glTexCoord2f(t2.x, t2.y);
+
+			if(this.hasNormals)
+			{
+				Vector3f n2 = this.normals.get((int) face.normal.y - 1);
+				glNormal3f(n2.x, n2.y, n2.z);
+			}
+			if(this.hasTextures)
+			{
+				Vector2f t2 = this.tetxures.get((int)face.texture.y - 1);
+				glTexCoord2f(t2.x, t2.y);
+			}
 			Vector3f v2 = this.vertices.get((int) face.vertex.y - 1);
 			glVertex3f(v2.x, v2.y, v2.z);
 
-			Vector3f n3 = this.normals.get((int) face.normal.z - 1);
-			glNormal3f(n3.x, n3.y, n3.z);
-			Vector2f t3 = this.tetxures.get((int)face.texture.z - 1);
-			glTexCoord2f(t3.x, t3.y);
+			if(this.hasNormals)
+			{
+				Vector3f n3 = this.normals.get((int) face.normal.z - 1);
+				glNormal3f(n3.x, n3.y, n3.z);
+			}
+			if(this.hasTextures)
+			{
+				Vector2f t3 = this.tetxures.get((int)face.texture.z - 1);
+				glTexCoord2f(t3.x, t3.y);
+			}
 			Vector3f v3 = this.vertices.get((int) face.vertex.z - 1);
 			glVertex3f(v3.x, v3.y, v3.z);
 		}
--- BOLT/src/render/util/OBJLoader.java
+++ BOLT/src/render/util/OBJLoader.java
@@ -5,6 +5,7 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 
+import org.lwjgl.util.vector.Vector2f;
 import org.lwjgl.util.vector.Vector3f;
 
 import render.Face;
@@ -37,21 +38,50 @@ public class OBJLoader
 				
 				m.normals.add(new Vector3f(x, y, z));
 			}
+			else if(line.startsWith("vt "))
+			{
+				float x = Float.valueOf(line.split(" ")[1]);
+				float y = Float.valueOf(line.split(" ")[2]);
+				
+				m.tetxures.add(new Vector2f(x, y));
+			}
 			else if(line.startsWith("f "))
 			{
-				float x = Float.valueOf(line.split(" ")[1].split("/")[0]);
-				float y = Float.valueOf(line.split(" ")[2].split("/")[0]);
-				float z = Float.valueOf(line.split(" ")[3].split("/")[0]);
+				String s1 = line.split(" ")[1], s2 = line.split(" ")[2], s3 = line.split(" ")[3];				
+				
+				float x = Float.valueOf(s1.split("/")[0]);
+				float y = Float.valueOf(s2.split("/")[0]);
+				float z = Float.valueOf(s3.split("/")[0]);
 				
 				Vector3f vertexIndices = new Vector3f(x, y, z);
+				Vector3f normalIndices = new Vector3f(0, 0, 0);
+				Vector3f textureIndices = new Vector3f(0, 0, 0);
+				
+				try
+				{
+					
+					x = Float.valueOf(s1.split("/")[1]);
+					y = Float.valueOf(s2.split("/")[1]);
+					z = Float.valueOf(s3.split("/")[1]);
+					
+					textureIndices = new Vector3f(x, y, z);
+					m.hasTextures = true;
+				}
+				catch(Exception e){};
 
-				x = Float.valueOf(line.split(" ")[1].split("/")[2]);
-				y = Float.valueOf(line.split(" ")[2].split("/")[2]);
-				z = Float.valueOf(line.split(" ")[3].split("/")[2]);
+				try
+				{
+					x = Float.valueOf(s1.split("/")[2]);
+					y = Float.valueOf(s2.split("/")[2]);
+					z = Float.valueOf(s3.split("/")[2]);
+					
+					normalIndices = new Vector3f(x, y, z);
+					m.hasNormals = true;
+				}
+				catch(Exception e){};
 				
-				Vector3f normalIndices = new Vector3f(x, y, z);
 				
-				m.faces.add(new Face(vertexIndices, normalIndices));
+				m.faces.add(new Face(vertexIndices, normalIndices, textureIndices));
 			}
 		}
 		reader.close();

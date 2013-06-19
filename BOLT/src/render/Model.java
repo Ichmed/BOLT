2dbf868aae18f4079ff56504091a1ee29a24444b
Ichmed <mijomeyer@t-online.de>
2013-06-19 14:30:56 +0200

    Revert "Textured Faces"
    This reverts commit 7461b3e8305cd87aeda9d81b74c3634e588ed312.

--- BOLT/src/render/Model.java
+++ BOLT/src/render/Model.java
@@ -5,14 +5,12 @@ import static org.lwjgl.opengl.GL11.*;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.lwjgl.util.vector.Vector2f;
 import org.lwjgl.util.vector.Vector3f;
 
 public class Model
 {
 	public List<Vector3f> vertices = new ArrayList<Vector3f>();
 	public List<Vector3f> normals = new ArrayList<Vector3f>();
-	public List<Vector2f> tetxures = new ArrayList<Vector2f>();
 
 	public List<Face> faces = new ArrayList<Face>();
 
@@ -27,22 +25,16 @@ public class Model
 		{
 			Vector3f n1 = this.normals.get((int) face.normal.x - 1);
 			glNormal3f(n1.x, n1.y, n1.z);
-			Vector2f t1 = this.tetxures.get((int)face.texture.x - 1);
-			glTexCoord2f(t1.x, t1.y);
 			Vector3f v1 = this.vertices.get((int) face.vertex.x - 1);
 			glVertex3f(v1.x, v1.y, v1.z);
 
 			Vector3f n2 = this.normals.get((int) face.normal.y - 1);
 			glNormal3f(n2.x, n2.y, n2.z);
-			Vector2f t2 = this.tetxures.get((int)face.texture.y - 1);
-			glTexCoord2f(t2.x, t2.y);
 			Vector3f v2 = this.vertices.get((int) face.vertex.y - 1);
 			glVertex3f(v2.x, v2.y, v2.z);
 
 			Vector3f n3 = this.normals.get((int) face.normal.z - 1);
 			glNormal3f(n3.x, n3.y, n3.z);
-			Vector2f t3 = this.tetxures.get((int)face.texture.z - 1);
-			glTexCoord2f(t3.x, t3.y);
 			Vector3f v3 = this.vertices.get((int) face.vertex.z - 1);
 			glVertex3f(v3.x, v3.y, v3.z);
 		}

7461b3e8305cd87aeda9d81b74c3634e588ed312
Ichmed <mijomeyer@t-online.de>
2013-06-19 14:30:23 +0200

    Textured Faces

--- BOLT/src/render/Face.java
+++ BOLT/src/render/Face.java
@@ -6,10 +6,17 @@ public class Face
 {
 	public Vector3f vertex = new Vector3f();
 	public Vector3f normal = new Vector3f();
+	public Vector3f texture = new Vector3f();
 	
 	public Face(Vector3f vert, Vector3f norm)
 	{
+		this(vert, norm, new Vector3f(0, 0, 0));
+	}
+	
+	public Face(Vector3f vert, Vector3f norm, Vector3f tex)
+	{
 		this.vertex = vert;
 		this.normal = norm;
+		this.texture = tex;
 	}
 }

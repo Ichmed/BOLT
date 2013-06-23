83f5fe3f8a58c03053d2db21dc6a7f6ba08f0d85
Ichmed <mijomeyer@t-online.de>
2013-06-23 03:15:17 +0200

    Test Files

--- BOLT/src/Main.java
+++ BOLT/src/Main.java
@@ -2,24 +2,30 @@ import static org.lwjgl.util.glu.GLU.gluPerspective;
 import static org.lwjgl.opengl.GL11.*;
 
 import java.io.IOException;
+import java.nio.FloatBuffer;
 
+import org.lwjgl.BufferUtils;
 import org.lwjgl.LWJGLException;
 import org.lwjgl.opengl.Display;
+import org.lwjgl.opengl.DisplayMode;
 
 import render.Model;
-import render.RenderHelper;
+import render.util.Misc;
 import render.util.OBJLoader;
+import render.util.ShaderLoader;
 
 public class Main
 {
 	public static Model m;
 
+	static int i = 0;
+
 	public static void main(String[] args)
 	{
 		try
 		{
-			Display.setDisplayMode(Display.getDesktopDisplayMode());
-			Display.setFullscreen(true);
+			Display.setDisplayMode(new DisplayMode(640, 480));
+			Display.setDisplayModeAndFullscreen(Display.getDesktopDisplayMode());
 			Display.create();
 		}
 		catch (LWJGLException e)
@@ -29,7 +35,7 @@ public class Main
 
 		try
 		{
-			m = OBJLoader.loadModel("test/crystal.obj");
+			m = OBJLoader.loadModel("test/", "crystal.obj");
 		}
 		catch (IOException e)
 		{
@@ -41,7 +47,20 @@ public class Main
 		gluPerspective(100, (float) Display.getWidth() / Display.getHeight(), 0.01f, 1000);
 		glMatrixMode(GL_MODELVIEW);
 
-		glScaled(0.005d, 0.005d, 0.005d);
+		glShadeModel(GL_SMOOTH);
+		glEnable(GL_DEPTH_TEST);
+		glEnable(GL_CULL_FACE);
+		
+		glEnable(GL_LIGHTING);
+		glEnable(GL_LIGHT0);
+		glLightModel(GL_LIGHT_MODEL_AMBIENT, Misc.asFloatBuffer(new float[]{0.1f, 0.1f, 0.1f, 1f}));
+		glLight(GL_LIGHT0, GL_DIFFUSE, Misc.asFloatBuffer(new float[]{1.5f, 1.5f, 1.5f, 1}));
+		glEnable(GL_COLOR_MATERIAL);
+		glColorMaterial(GL_FRONT, GL_DIFFUSE);
+		glMaterialf(GL_FRONT, GL_SHININESS, 10f);
+		
+		ShaderLoader.useProgram("test/shader");
+		
 		while (!Display.isCloseRequested())
 			gameLoop();
 
@@ -50,26 +69,29 @@ public class Main
 
 	public static void gameLoop()
 	{
+		i++;
 		glPushMatrix();
 
-		glEnable(GL_BLEND);
-		glEnable(GL_DEPTH_TEST); 
-
-		glEnable(GL_TEXTURE_2D);
-
-		glColor4d(1, 1, 1, 0.5d);
-
-		RenderHelper.bindTexture("test/crystal.png");
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-		glTranslated(0, 0, -5);
-		// glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+		glEnable(GL_BLEND);
+		glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+
+		glLight(GL_LIGHT0, GL_POSITION, Misc.asFloatBuffer(new float[]{(float) Math.sin(i / 1000d), (float) Math.sin(i / 100d), (float) Math.cos(i / 1000d), 1}));
+		
+		glEnable(GL_TEXTURE_2D);
+		glTranslated(0, 0, -7);
+		glRotated(i, 0, 1, 0);
+		m.renderModel();
+		glTranslated(0, 0, 2);
+		glTranslated(0, (Math.sin(i / 100d) * 2) - 1, 0);
+
+//		 glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
 		m.renderModel();
 
 		Display.update();
 		Display.sync(50);
 
 		glPopMatrix();
-
 	}
 }
---
+++ BOLT/test/shader.vert
@@ -0,0 +1,27 @@
+varying vec3 color;
+
+void main()
+{
+    gl_TexCoord[0] = gl_TextureMatrix[0] * gl_MultiTexCoord0; 
+    
+    vec3 vertexPosition = (gl_ModelViewMatrix * gl_Vertex).xyz;
+    vec3 lightDirection = normalize(gl_LightSource[0].position.xyz - vertexPosition);    
+    vec3 surfaceNormal = (gl_NormalMatrix * gl_Normal).xyz;
+    
+    float diffuseLightIntensity = max(0, dot(surfaceNormal, lightDirection));
+    
+    color.rgb = diffuseLightIntensity * gl_FrontMaterial.diffuse.rgb;
+    color += gl_LightModel.ambient.rgb;
+    
+	vec3 reflectionDirection = normalize(reflect(-lightDirection, surfaceNormal));
+    
+    float specular = max(0.0, dot(surfaceNormal, reflectionDirection));
+    
+    if(diffuseLightIntensity != 0)
+    {
+		float fspecular = pow(specular, gl_FrontMaterial.shininess);
+        color.rgb += vec3(fspecular, fspecular, fspecular);
+    }
+    
+    gl_Position = gl_ModelViewProjectionMatrix * gl_Vertex;
+}

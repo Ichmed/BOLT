a6f157aa59f339d5fd4eeb2fdee433595616ace9
Pr0t0typ3 <f.sch.prototype@gmail.com>
2013-06-23 15:17:28 +0200

    GameRules

--- BOLT/src/game/GameRules.java
+++ BOLT/src/game/GameRules.java
@@ -1,7 +1,51 @@
 package game;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
+import org.lwjgl.opengl.Display;
+
 public class GameRules
 {
-	/* Gravity in m/s^2 */
+	// Gravity in m/s^2
 	public static float gravity = 9.8f;
+	//MaxSpeed in m/s
+	public static float playerMaxSpeed = 2.77f;
+	
+	//load game.rules to static variables
+	public static void loadRules()
+	{
+		File gravityLoader = new File("game.rules");
+		try
+		{
+			BufferedReader reader = new BufferedReader(new FileReader(gravityLoader));
+			ArrayList<String> rulesIdentifier = new ArrayList<String>();
+			ArrayList<Float> rulesValues = new ArrayList<Float>();
+			String line;
+			while((line = reader.readLine()) != null)
+			{
+				String identifier = line.split("=")[0];
+				float value = Float.parseFloat(line.split("=")[1]);
+				rulesIdentifier.add(identifier);
+				rulesValues.add(value);
+			}
+			reader.close();
+			for(int i = 0; i < rulesIdentifier.size(); i++)
+			{
+				if (rulesIdentifier.get(i) == "gravity")
+					gravity = rulesValues.get(i);
+				else if (rulesIdentifier.get(i) == "playerMaxSpeed")
+					playerMaxSpeed = rulesValues.get(i);
+			}
+		}
+		catch (IOException e)
+		{
+			System.err.println("Error reading game rule file!\nGame will shut down now.");
+			Display.destroy();
+			System.exit(1);
+		}
+	}
 }

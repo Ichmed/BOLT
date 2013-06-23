c4752df987feba1027f872b77ffa5f1f312a3208
Ichmed <mijomeyer@t-online.de>
2013-06-23 02:50:37 +0200

    Shaders

---
+++ BOLT/src/render/util/Misc.java
@@ -0,0 +1,16 @@
+package render.util;
+
+import java.nio.FloatBuffer;
+
+import org.lwjgl.BufferUtils;
+
+public class Misc
+{	
+	public static FloatBuffer asFloatBuffer(float[] fs)
+	{
+		FloatBuffer buffer = BufferUtils.createFloatBuffer(fs.length);
+		buffer.put(fs);
+		buffer.flip();
+		return buffer;
+	}
+}

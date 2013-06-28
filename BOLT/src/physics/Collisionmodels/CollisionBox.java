680e0ccbd173ddb495e1e31d7a8f14757441e15d
Pr0t0typ3 <f.sch.prototype@gmail.com>
2013-06-28 16:12:41 +0200

    Pyth + Vectorrotation

--- BOLT/src/physics/Collisionmodels/CollisionBox.java
+++ BOLT/src/physics/Collisionmodels/CollisionBox.java
@@ -30,28 +30,28 @@ public class CollisionBox
 		this.mass = mass;
 	}
 	
-	public CollisionBox(Vector3f... dots)
+	public CollisionBox(Vector3f... points)
 	{
-		float minX = dots[0].x;
-		float minY = dots[0].y;
-		float minZ = dots[0].z;
-		float maxX = dots[0].x;
-		float maxY = dots[0].y;
-		float maxZ = dots[0].z;
-		for(int a = 0; a < dots.length; a++)
+		float minX = points[0].x;
+		float minY = points[0].y;
+		float minZ = points[0].z;
+		float maxX = points[0].x;
+		float maxY = points[0].y;
+		float maxZ = points[0].z;
+		for(int a = 0; a < points.length; a++)
 		{
-			if(dots[a].x < minX)
-				minX = dots[a].x;
-			else if(dots[a].x > maxX)
-				maxX = dots[a].x;
-			if(dots[a].y < minY)
-				minY = dots[a].y;
-			else if(dots[a].y > maxY)
-				maxY = dots[a].y;
-			if(dots[a].z < minZ)
-				minZ = dots[a].z;
-			else if(dots[a].z > maxZ)
-				maxZ = dots[a].z;
+			if(points[a].x < minX)
+				minX = points[a].x;
+			else if(points[a].x > maxX)
+				maxX = points[a].x;
+			if(points[a].y < minY)
+				minY = points[a].y;
+			else if(points[a].y > maxY)
+				maxY = points[a].y;
+			if(points[a].z < minZ)
+				minZ = points[a].z;
+			else if(points[a].z > maxZ)
+				maxZ = points[a].z;
 		}
 		startingPoint = new Vector3f(minX, minY, minZ);
 		width = new Vector3f(maxX - minX, 0, 0);
@@ -61,79 +61,87 @@ public class CollisionBox
 		Vector3f.add ((Vector3f)middle, (Vector3f)height.scale (0.5f), middle);
 	}
 	
-	public CollisionBox (float mass, Vector3f... dots)
+	public CollisionBox (float mass, Vector3f... points)
 	{
-		this(dots);
+		this(points);
 		this.mass = mass;
 	}
 	
-	public static CollisionBox createCollisionBox(Vector3f... dots)
+	//TODO ->
+	
+	public static CollisionBox createCollisionBox(Vector3f... points)
 	{
-		float minX = dots[0].x;
-		float minY = dots[0].y;
-		float minZ = dots[0].z;
-		float maxX = dots[0].x;
-		float maxY = dots[0].y;
-		float maxZ = dots[0].z;
+		float minX = points[0].x;
+		float minY = points[0].y;
+		float minZ = points[0].z;
+		float maxX = points[0].x;
+		float maxY = points[0].y;
+		float maxZ = points[0].z;
 		Vector3f dotMinX = new Vector3f();
 		Vector3f dotMinY = new Vector3f();
 		Vector3f dotMinZ = new Vector3f();
 		Vector3f dotMaxX = new Vector3f();
 		Vector3f dotMaxY = new Vector3f();
 		Vector3f dotMaxZ = new Vector3f();
-		for(int a = 0; a < dots.length; a++)
+		for(int a = 0; a < points.length; a++)
 		{
-			if(dots[a].x < minX)
+			if(points[a].x < minX)
 			{
-				minX = dots[a].x;
-				dotMinX =dots[a];
+				minX = points[a].x;
+				dotMinX =points[a];
 			}
-			else if(dots[a].x > maxX)
+			else if(points[a].x > maxX)
 			{
-				maxX = dots[a].x;
-				dotMaxX =dots[a];
+				maxX = points[a].x;
+				dotMaxX =points[a];
 			}
-			if(dots[a].y < minY)
+			if(points[a].y < minY)
 			{
-				minY = dots[a].y;
-				dotMinY =dots[a];
+				minY = points[a].y;
+				dotMinY =points[a];
 			}
-			else if(dots[a].y > maxY)
+			else if(points[a].y > maxY)
 			{
-				maxY = dots[a].y;
-				dotMaxY =dots[a];
+				maxY = points[a].y;
+				dotMaxY =points[a];
 			}
-			if(dots[a].z < minZ)
+			if(points[a].z < minZ)
 			{
-				minZ = dots[a].z;
-				dotMinZ =dots[a];
+				minZ = points[a].z;
+				dotMinZ =points[a];
 			}
-			else if(dots[a].z > maxZ)
+			else if(points[a].z > maxZ)
 			{
-				maxZ = dots[a].z;
-				dotMaxZ =dots[a];
+				maxZ = points[a].z;
+				dotMaxZ =points[a];
 			}
 		}
 		Vector3f bestNormalFront = new Vector3f(0, maxY - minY, 0);
-		bestNormalFront.normalise ();
 		Vector3f bestNormalLeft = new Vector3f(maxX - minX, 0, 0);
-		bestNormalLeft.normalise ();
 		Vector3f bestNormalTop = new Vector3f(0, 0, maxZ - minZ);
-		bestNormalTop.normalise ();
 		float distanceFrontBack = maxY - minY;
 		float distanceLeftRight = maxX - minX;
 		float distanceTopBottom = maxZ - minZ;
+		Vector3f NormalFront = new Vector3f(0, maxY - minY, 0);
+		Vector3f NormalLeft = new Vector3f(maxX - minX, 0, 0);
+		Vector3f NormalTop = new Vector3f(0, 0, maxZ - minZ);
+		bestNormalFront.normalise ();
+		bestNormalLeft.normalise ();
+		bestNormalTop.normalise ();
+		NormalFront.normalise ();
+		NormalLeft.normalise ();
+		NormalTop.normalise ();
 		Plane front = new Plane(bestNormalFront, dotMinY);
-		front.TransformToHesseNormalForm ();
 		Plane back = new Plane(bestNormalFront, dotMaxY);
-		back.TransformToHesseNormalForm ();
 		Plane left = new Plane(bestNormalLeft, dotMaxX);
-		left.TransformToHesseNormalForm ();
 		Plane right = new Plane(bestNormalLeft, dotMinX);
-		right.TransformToHesseNormalForm ();
 		Plane top = new Plane(bestNormalTop, dotMaxZ);
-		top.TransformToHesseNormalForm ();
 		Plane bottom = new Plane(bestNormalTop, dotMinZ);
+		front.TransformToHesseNormalForm ();
+		back.TransformToHesseNormalForm ();
+		left.TransformToHesseNormalForm ();
+		right.TransformToHesseNormalForm ();
+		top.TransformToHesseNormalForm ();
 		bottom.TransformToHesseNormalForm ();
 		return new CollisionBox();
 	}
--- BOLT/src/util/MathHelper.java
+++ BOLT/src/util/MathHelper.java
@@ -1,32 +1,37 @@
 package util;
 
-import org.lwjgl.util.vector.Vector2f;
+import java.nio.FloatBuffer;
+
+import org.lwjgl.util.vector.Matrix3f;
 import org.lwjgl.util.vector.Vector3f;
-import org.lwjgl.util.vector.Vector4f;
 
 public abstract class MathHelper
 {
-	public static Vector2f normalize(Vector2f v)
+	//TODO: Intersection between a plane and a graph
+	
+	public static float calculateDistancePointPlane(Vector3f point, Plane hesseNormalForm)
 	{
-		v.scale(1 / v.length());
-		return v;
+		return (hesseNormalForm.normal.x * point.x + hesseNormalForm.normal.y * point.y + hesseNormalForm.normal.z * point.z - hesseNormalForm.startingPoint.x * hesseNormalForm.normal.x -
+				hesseNormalForm.startingPoint.y * hesseNormalForm.normal.y - hesseNormalForm.startingPoint.z * hesseNormalForm.normal.z);
 	}
 	
-	public static Vector3f normalize(Vector3f v)
+	public static double pyth(float a, float b)
 	{
-		v.scale(1 / v.length());
-		return v;
+		return Math.sqrt (a * a + b * b);
 	}
 	
-	public static Vector4f normalize(Vector4f v)
+	public static void rotateVector(Vector3f vector, float degree, Plane rotationPlane)
 	{
-		v.scale(1 / v.length());
-		return v;
+		Matrix3f rotationMatrix = new Matrix3f();
+		rotationMatrix.m00 = (float)(rotationPlane.normal.x * rotationPlane.normal.x * (1 - Math.cos (degree)) + Math.cos (degree));
+		rotationMatrix.m01 = (float)(rotationPlane.normal.x * rotationPlane.normal.y * (1 - Math.cos (degree)) - rotationPlane.normal.z * Math.sin (degree));
+		rotationMatrix.m02 = (float)(rotationPlane.normal.x * rotationPlane.normal.z * (1 - Math.cos (degree)) + rotationPlane.normal.y * Math.cos (degree));
+		rotationMatrix.m10 = (float)(rotationPlane.normal.x * rotationPlane.normal.y * (1 - Math.cos (degree)) + rotationPlane.normal.z * Math.cos (degree));
+		rotationMatrix.m11 = (float)(rotationPlane.normal.y * rotationPlane.normal.y * (1 - Math.cos (degree)) + Math.cos (degree));
+		rotationMatrix.m12 = (float)(rotationPlane.normal.y * rotationPlane.normal.z * (1 - Math.cos (degree)) + rotationPlane.normal.x * Math.cos (degree));
+		rotationMatrix.m20 = (float)(rotationPlane.normal.x * rotationPlane.normal.z * (1 - Math.cos (degree)) + rotationPlane.normal.y * Math.cos (degree));
+		rotationMatrix.m21 = (float)(rotationPlane.normal.z * rotationPlane.normal.y * (1 - Math.cos (degree)) + rotationPlane.normal.x * Math.cos (degree));
+		rotationMatrix.m22 = (float)(rotationPlane.normal.z * rotationPlane.normal.z * (1 - Math.cos (degree)) + Math.cos (degree));
+		Matrix3f.transform (rotationMatrix, vector, vector);
 	}
-	
-	/*TODO: Intersection between a plane and a graph
-	public static Vector3f calculateIntersection()
-	{
-		
-	}*/
 }

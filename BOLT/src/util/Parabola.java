a24a36888174e3b66f3326cad8ea3f8a09b24f55
Marcel Mundl <Marcel@Mundlhome.de>
2013-07-16 17:05:25 +0200

    Parabola added added new Class Parabola.java for collision calculation

---
+++ BOLT/src/util/Parabola.java
@@ -0,0 +1,21 @@
+package util;
+
+import org.lwjgl.util.vector.Vector3f;
+
+/**
+ * a parabola in 3d to calculate collisions
+ * @author marcel
+ */
+public class Parabola {
+
+	/**
+	 * creates a parabola based on 2 vectors
+	 * @param a a vector pointing at the starting point
+	 * @param b the vector multiplied with u
+	 * @param c the vector multiplied with u²
+	 */
+	public Parabola(Vector3f a, Vector3f b, Vector3f c) {
+		 
+	}
+
+}

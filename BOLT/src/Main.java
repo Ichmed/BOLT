5c76ed26f40814c3e796120179bbe9e38783de22
Ichmed <mijomeyer@t-online.de>
2013-07-16 17:42:43 +0200

    added fullscreen toggle
    Don't use it ;P

--- BOLT/src/Main.java
+++ BOLT/src/Main.java
@@ -1,37 +1,4 @@
-import static org.lwjgl.opengl.GL11.GL_BLEND;
-import static org.lwjgl.opengl.GL11.GL_COLOR_BUFFER_BIT;
-import static org.lwjgl.opengl.GL11.GL_COLOR_MATERIAL;
-import static org.lwjgl.opengl.GL11.GL_CULL_FACE;
-import static org.lwjgl.opengl.GL11.GL_DEPTH_BUFFER_BIT;
-import static org.lwjgl.opengl.GL11.GL_DEPTH_TEST;
-import static org.lwjgl.opengl.GL11.GL_DIFFUSE;
-import static org.lwjgl.opengl.GL11.GL_FRONT;
-import static org.lwjgl.opengl.GL11.GL_LIGHT0;
-import static org.lwjgl.opengl.GL11.GL_LIGHTING;
-import static org.lwjgl.opengl.GL11.GL_LIGHT_MODEL_AMBIENT;
-import static org.lwjgl.opengl.GL11.GL_MODELVIEW;
-import static org.lwjgl.opengl.GL11.GL_ONE_MINUS_SRC_ALPHA;
-import static org.lwjgl.opengl.GL11.GL_POSITION;
-import static org.lwjgl.opengl.GL11.GL_PROJECTION;
-import static org.lwjgl.opengl.GL11.GL_SHININESS;
-import static org.lwjgl.opengl.GL11.GL_SMOOTH;
-import static org.lwjgl.opengl.GL11.GL_SRC_ALPHA;
-import static org.lwjgl.opengl.GL11.GL_TEXTURE_2D;
-import static org.lwjgl.opengl.GL11.glBlendFunc;
-import static org.lwjgl.opengl.GL11.glClear;
-import static org.lwjgl.opengl.GL11.glColorMaterial;
-import static org.lwjgl.opengl.GL11.glEnable;
-import static org.lwjgl.opengl.GL11.glLight;
-import static org.lwjgl.opengl.GL11.glLightModel;
-import static org.lwjgl.opengl.GL11.glLoadIdentity;
-import static org.lwjgl.opengl.GL11.glMaterialf;
-import static org.lwjgl.opengl.GL11.glMatrixMode;
-import static org.lwjgl.opengl.GL11.glPopMatrix;
-import static org.lwjgl.opengl.GL11.glPushMatrix;
-import static org.lwjgl.opengl.GL11.glRotated;
-import static org.lwjgl.opengl.GL11.glShadeModel;
-import static org.lwjgl.opengl.GL11.glTranslated;
-import static org.lwjgl.opengl.GL11.glTranslatef;
+import static org.lwjgl.opengl.GL11.*;
 import static org.lwjgl.util.glu.GLU.gluPerspective;
 import game.Camera;
 
@@ -42,6 +9,7 @@ import java.io.FileReader;
 import java.io.IOException;
 
 import org.lwjgl.LWJGLException;
+import org.lwjgl.input.Keyboard;
 import org.lwjgl.input.Mouse;
 import org.lwjgl.opengl.Display;
 import org.lwjgl.opengl.DisplayMode;
@@ -59,16 +27,15 @@ public class Main
 
 	static int i = 0;
 	static Camera camera = new Camera();
-	
+
 	public static int cameraSpeed;
 
 	public static void main(String[] args)
 	{
-		//TODO: Catch arguments for Console
-		
+		// TODO: Catch arguments for Console
+
 		try
 		{
-			Display.setDisplayMode(new DisplayMode(640, 480));
 			Display.setDisplayModeAndFullscreen(Display.getDesktopDisplayMode());
 			Display.create();
 		}
@@ -85,30 +52,11 @@ public class Main
 		{
 			e.printStackTrace();
 		}
-		
+
 		loadOptions();
-
-		glMatrixMode(GL_PROJECTION);
-		glLoadIdentity();
-		gluPerspective(100, (float) Display.getWidth() / Display.getHeight(), 0.01f, 1000);
-		glMatrixMode(GL_MODELVIEW);
-
-		glShadeModel(GL_SMOOTH);
-		glEnable(GL_DEPTH_TEST);
-		glEnable(GL_CULL_FACE);
-
-		glEnable(GL_LIGHTING);
-		glEnable(GL_LIGHT0);
-		glLightModel(GL_LIGHT_MODEL_AMBIENT, Misc.asFloatBuffer(new float[] { 0.1f, 0.1f, 0.1f, 1f }));
-		glLight(GL_LIGHT0, GL_DIFFUSE, Misc.asFloatBuffer(new float[] { 1.5f, 1.5f, 1.5f, 1 }));
-		glEnable(GL_COLOR_MATERIAL);
-		glColorMaterial(GL_FRONT, GL_DIFFUSE);
-		glMaterialf(GL_FRONT, GL_SHININESS, 10f);
 		
-		glLightModel(GL_LIGHT_MODEL_AMBIENT, Misc.asFloatBuffer(new float[]{0.1f, 0.1f, 0.1f, 1}));	
+		initGLSettings();		
 
-		ShaderLoader.useProgram("test/", "shader");
-		
 		Mouse.setCursorPosition(Display.getWidth() / 2, Display.getHeight() / 2);
 		Mouse.setGrabbed(true);
 
@@ -121,32 +69,49 @@ public class Main
 	public static void gameLoop()
 	{
 		i++;
-		
-		camera.rotation.y += ((Mouse.getX() - (Display.getWidth() / 2)) / (float)Display.getWidth()) * cameraSpeed;
-		camera.rotation.x -= ((Mouse.getY() - (Display.getHeight() / 2)) / (float)Display.getHeight()) * cameraSpeed;
-		
+
+		camera.rotation.y += ((Mouse.getX() - (Display.getWidth() / 2)) / (float) Display.getWidth()) * cameraSpeed;
+		camera.rotation.x -= ((Mouse.getY() - (Display.getHeight() / 2)) / (float) Display.getHeight()) * cameraSpeed;
+
 		camera.rotation.x = MathHelper.clamp(camera.rotation.x, -90, 90);
-		
+
 		Mouse.setCursorPosition(Display.getWidth() / 2, Display.getHeight() / 2);
-		
-		Vector3f rot = new Vector3f((float)Math.cos(camera.rotation.x), (float)Math.cos(camera.rotation.y), (float)Math.cos(camera.rotation.z));
-		
-		//TODO camera movement
-//		if(Keyboard.isKeyDown(Keyboard.KEY_W))
-//		{
-//			camera.position.x += rot.x;
-//			camera.position.y += rot.y;
-//			camera.position.z += rot.z;
-//		}
-		
+
+		Vector3f rot = new Vector3f((float) Math.cos(camera.rotation.x), (float) Math.cos(camera.rotation.y), (float) Math.cos(camera.rotation.z));
+
+		// TODO camera movement
+		if (Keyboard.isKeyDown(Keyboard.KEY_F11))
+		{
+			try
+			{
+				Display.destroy();
+				if (Display.isFullscreen())
+				{
+					Display.setFullscreen(false);
+					Display.setDisplayMode(new DisplayMode(640, 480));
+				}
+				else 
+				{
+					Display.setDisplayModeAndFullscreen(Display.getDesktopDisplayMode());
+				}
+				Display.create();
+			}
+			catch (LWJGLException e)
+			{
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			initGLSettings();
+			Mouse.setCursorPosition(Display.getWidth() / 2, Display.getHeight() / 2);
+		}
+
 		glPushMatrix();
-		
+
 		glRotated(camera.rotation.x, 1f, 0f, 0f);
 		glRotated(camera.rotation.y, 0f, 1f, 0f);
 		glRotated(camera.rotation.z, 0f, 0f, 1f);
 		glTranslatef(-camera.position.x, -camera.position.y, -camera.position.z);
-		
-		
+
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
 		glEnable(GL_BLEND);
@@ -175,6 +140,30 @@ public class Main
 		glPopMatrix();
 	}
 	
+	public static void initGLSettings()
+	{
+		glMatrixMode(GL_PROJECTION);
+		glLoadIdentity();
+		gluPerspective(100, (float) Display.getWidth() / Display.getHeight(), 0.01f, 1000);
+		glMatrixMode(GL_MODELVIEW);
+
+		glShadeModel(GL_SMOOTH);
+		glEnable(GL_DEPTH_TEST);
+		glEnable(GL_CULL_FACE);
+
+		glEnable(GL_LIGHTING);
+		glEnable(GL_LIGHT0);
+		glLightModel(GL_LIGHT_MODEL_AMBIENT, Misc.asFloatBuffer(new float[] { 0.1f, 0.1f, 0.1f, 1f }));
+		glLight(GL_LIGHT0, GL_DIFFUSE, Misc.asFloatBuffer(new float[] { 1.5f, 1.5f, 1.5f, 1 }));
+		glEnable(GL_COLOR_MATERIAL);
+		glColorMaterial(GL_FRONT, GL_DIFFUSE);
+		glMaterialf(GL_FRONT, GL_SHININESS, 10f);
+
+		glLightModel(GL_LIGHT_MODEL_AMBIENT, Misc.asFloatBuffer(new float[] { 0.1f, 0.1f, 0.1f, 1 }));
+		
+		ShaderLoader.useProgram("test/", "shader");
+	}
+
 	public static void loadOptions()
 	{
 		File OBJFile = new File("nonsync/options.txt");
@@ -182,25 +171,21 @@ public class Main
 		try
 		{
 			BufferedReader reader = new BufferedReader(new FileReader(OBJFile));
-			while((line = reader.readLine()) != null)
+			while ((line = reader.readLine()) != null)
 			{
-				if(line.startsWith("cameraSpeed"))
-					cameraSpeed = Integer.valueOf(line.split("=")[1]);
+				if (line.startsWith("cameraSpeed")) cameraSpeed = Integer.valueOf(line.split("=")[1]);
 			}
 		}
 		catch (FileNotFoundException e1)
 		{
-			// TODO Auto-generated catch block
 			e1.printStackTrace();
 		}
 		catch (NumberFormatException e)
 		{
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		catch (IOException e)
 		{
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}

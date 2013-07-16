0e604a396ceac5bee9aa9aa48f3abb356ae582f2
Marcel Mundl <Marcel@Mundlhome.de>
2013-07-16 17:42:40 +0200

    progress in parabola

--- BOLT/src/util/Parabola.java
+++ BOLT/src/util/Parabola.java
@@ -8,6 +8,10 @@ import org.lwjgl.util.vector.Vector3f;
  */
 public class Parabola {
 
+	Vector3f startpoint = null;
+	Vector3f dir = null;
+	Vector3f inf = null;
+	
 	/**
 	 * creates a parabola based on 2 vectors
 	 * @param a a vector pointing at the starting point
@@ -15,7 +19,21 @@ public class Parabola {
 	 * @param c the vector multiplied with u²
 	 */
 	public Parabola(Vector3f a, Vector3f b, Vector3f c) {
-		 
+		 this.startpoint = a;
+		 this.dir = b;
+		 this.inf = c;
 	}
 
+	/**
+	 * gives you a point from the graph by using the parameter
+	 * @param u the parameter
+	 * @return
+	 */
+	public Vector3f getPoint(float u){
+		Vector3f result = new Vector3f();
+		result.setX(startpoint.getX() + u * dir.getX() + u * u * inf.getX());
+		result.setY(startpoint.getY() + u * dir.getY() + u * u * inf.getY());
+		result.setZ(startpoint.getZ() + u * dir.getZ() + u * u * inf.getZ());
+		return result;
+	}
 }

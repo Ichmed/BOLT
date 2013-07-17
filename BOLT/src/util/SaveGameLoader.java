83b5c8622eb9e26295c77f3eaa0cf649d9305b24
Dakror <mail@dakror.de>
2013-07-17 18:48:13 +0200

    Added savegame loader

---
+++ BOLT/src/util/SaveGameLoader.java
@@ -0,0 +1,28 @@
+package util;
+
+import java.io.File;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class SaveGameLoader
+{
+  public static void loadSaveGame(String file)
+  {
+    try
+    {
+      JSONObject content = new JSONObject(Compressor.decompressFile(new File(file)));
+      // TODO: do something with this loaded data!
+    }
+    catch (JSONException e)
+    {
+      e.printStackTrace();
+    }
+  }
+  
+  public static void saveSaveGame(String file)
+  {
+    Compressor.compressFile(new File(file), "PUT_CONTENT_OF_SAVEGAME_HERE");
+    // TODO: do something usefull!
+  }
+}

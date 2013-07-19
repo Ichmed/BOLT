8df763a7bbdfdcbdd3b6fcd18b9fecd68b13372e
Ichmed <mijomeyer@t-online.de>
2013-07-19 12:40:31 +0200

    Update Face.java

--- BOLT/src/render/Face.java
+++ BOLT/src/render/Face.java
@@ -4,19 +4,12 @@ import org.lwjgl.util.vector.Vector3f;
 
 public class Face 
 {
-	public Vector3f vertex = new Vector3f();
-	public Vector3f normal = new Vector3f();
-	public Vector3f texture = new Vector3f();
+	public final Vector3f[] points;
+	int pointCount = 0;
 	
-	public Face(Vector3f vert, Vector3f norm)
+	public Face(Vector3f... vec)
 	{
-		this(vert, norm, new Vector3f(0, 0, 0));
-	}
-	
-	public Face(Vector3f vert, Vector3f norm, Vector3f tex)
-	{
-		this.vertex = vert;
-		this.normal = norm;
-		this.texture = tex;
+		points = vec;
+		this.pointCount = vec.length;
 	}
 }

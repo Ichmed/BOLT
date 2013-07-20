e593cf9269877774ad7042b281f1b255a7a9e154
Pr0t0typ3 <f.sch.prototype@gmail.com>
2013-07-21 01:51:28 +0200

    debuggin2

--- BOLT/src/physics/collisionmodels/CollisionBox.java
+++ BOLT/src/physics/collisionmodels/CollisionBox.java
@@ -4,6 +4,7 @@ import game.Main;
 
 import java.util.ArrayList;
 import java.util.logging.Level;
+import java.util.logging.LogRecord;
 
 import org.lwjgl.util.vector.Vector3f;
 
@@ -308,7 +309,14 @@ public class CollisionBox
 		back = new Plane(bestNormalFront, bestPointBack);
 		front.transformToHesseNormalForm();
 		back.transformToHesseNormalForm();
-		
+		String log = "";
+		for(Vector3f point : points)
+		{
+			log = log + "\n" + point.toString();
+			if(point.x == front.startingPoint.x && point.y == front.startingPoint.y && point.z == front.startingPoint.z)
+				Main.log.log(Level.INFO, point.toString());
+		}
+		Main.log.log(Level.INFO, log);
 		Main.log.log(Level.INFO, "front/back: normal[" + front.normal.x + ", " + front.normal.y + ", " + front.normal.z + "]\n");
 		Main.log.log(Level.INFO, "front: startingPoint[" + front.startingPoint.x + ", " + front.startingPoint.y + ", " + front.startingPoint.z + "]\n");
 		Main.log.log(Level.INFO, "back: startingPoint[" + back.startingPoint.x + ", " + back.startingPoint.y + ", " + back.startingPoint.z + "]\n");

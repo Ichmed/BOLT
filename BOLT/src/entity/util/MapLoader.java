09a9ebdffb76cd19ff04456633b624669a5d6a3a
Dakror <mail@dakror.de>
2013-07-21 15:04:01 +0200

    Added maploader support for Entity.rotation

--- BOLT/src/entity/util/MapLoader.java
+++ BOLT/src/entity/util/MapLoader.java
@@ -16,58 +16,60 @@ import util.Compressor;
 
 public class MapLoader
 {
-	public static World loadMap(String file)
-	{
-		try
-		{
-			JSONObject content = new JSONObject(Compressor.decompressFile(new File(file)));
-
-			World world = new World();
-
-			JSONArray entities = content.getJSONArray("entity");
-
-			for (int i = 0; i < entities.length(); i++)
-			{
-				JSONObject o = entities.getJSONObject(i);
-				Entity entity = EntityRegistry.createEntity(o.getString("name"));
-
-				JSONArray v = o.getJSONArray("pos");
-				entity.position = new Vector3f((float) v.getDouble(0), (float) v.getDouble(1), (float) v.getDouble(2));
-
-				HashMap<String, Object> customValues = new HashMap<>();
-				JSONObject c = o.getJSONObject("custom");
-				JSONArray cKeys = c.names();
-				for (int j = 0; j < c.length(); j++)
-				{
-					customValues.put(cKeys.getString(i), c.get(cKeys.getString(i)));
-				}
-				entity.customValues = customValues;
-
-				world.spawnEntity(entity);
-			}
-			return world;
-		}
-		catch (JSONException e)
-		{
-			e.printStackTrace();
-			return null;
-		}
-	}
-
-	public static JSONArray serializeVector3f(Vector3f v)
-	{
-		try
-		{
-			JSONArray a = new JSONArray();
-			a.put(v.x);
-			a.put(v.y);
-			a.put(v.z);
-			return a;
-		}
-		catch (JSONException e)
-		{
-			e.printStackTrace();
-			return null;
-		}
-	}
+  public static World loadMap(String file)
+  {
+    try
+    {
+      JSONObject content = new JSONObject(Compressor.decompressFile(new File(file)));
+      
+      World world = new World();
+      
+      JSONArray entities = content.getJSONArray("entity");
+      
+      for (int i = 0; i < entities.length(); i++)
+      {
+        JSONObject o = entities.getJSONObject(i);
+        Entity entity = EntityRegistry.createEntity(o.getString("name"));
+        
+        JSONArray pos = o.getJSONArray("pos");
+        JSONArray rot = o.getJSONArray("rot");
+        entity.setPosition((float) pos.getDouble(0), (float) pos.getDouble(1), (float) pos.getDouble(2));
+        entity.setRotation((float) rot.getDouble(0), (float) rot.getDouble(1), (float) rot.getDouble(2));
+        
+        HashMap<String, Object> customValues = new HashMap<>();
+        JSONObject c = o.getJSONObject("custom");
+        JSONArray cKeys = c.names();
+        for (int j = 0; j < c.length(); j++)
+        {
+          customValues.put(cKeys.getString(i), c.get(cKeys.getString(i)));
+        }
+        entity.customValues = customValues;
+        
+        world.spawnEntity(entity);
+      }
+      return world;
+    }
+    catch (JSONException e)
+    {
+      e.printStackTrace();
+      return null;
+    }
+  }
+  
+  public static JSONArray serializeVector3f(Vector3f v)
+  {
+    try
+    {
+      JSONArray a = new JSONArray();
+      a.put(v.x);
+      a.put(v.y);
+      a.put(v.z);
+      return a;
+    }
+    catch (JSONException e)
+    {
+      e.printStackTrace();
+      return null;
+    }
+  }
 }

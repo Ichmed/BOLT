bec44776914328d8d407098c146aaddd0e14f009
Ichmed <mijomeyer@t-online.de>
2013-07-29 15:38:22 +0200

    added staticProb funtionality

---
+++ BOLT/src/entity/probs/StaticProb.java
@@ -0,0 +1,14 @@
+package entity.probs;
+
+import entity.Entity;
+
+public class StaticProb extends Entity
+{
+	@Override
+	public void render()
+	{
+		this.model = (String)customValues.get("displayModel");
+		super.render();
+	}
+	
+}

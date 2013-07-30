fc4ce003b1747e999860aa5306360f87ad290a2e
Dakror <mail@dakror.de>
2013-07-30 14:32:50 +0200

    bugfix

--- BOLT/src/entity/util/MapLoader.java
+++ BOLT/src/entity/util/MapLoader.java
@@ -43,7 +43,7 @@ public class MapLoader
 					customValues.put(cKeys.getString(j), c.get(cKeys.getString(j)));
 				}
 				entity.customValues = customValues;
-				entity.key = c.getString("id");
+				entity.key = o.getString("id");
 
 				world.spawnEntity(entity);
 			}

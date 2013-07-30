8ce4b6cb7022d79034305c475c3be5cbbb3002cd
Ichmed <mijomeyer@t-online.de>
2013-07-30 18:37:56 +0200

    twub di wub

--- BOLT/src/entity/probs/StaticProb.java
+++ BOLT/src/entity/probs/StaticProb.java
@@ -14,19 +14,18 @@ public class StaticProb extends Entity
 	@Override
 	public void render()
 	{
-		this.model = (String)customValues.get("displayModel");
+		this.model = (String) customValues.get("displayModel");
 		super.render();
 	}
-	
+
 	protected void doRendering()
-	{		
+	{
 		glTranslated(this.position.x, this.position.y, this.position.z);
 		glRotated(this.rotation.x, 1, 0, 0);
 		glRotated(this.rotation.y, 0, 1, 0);
 		glRotated(this.rotation.z, 0, 0, 1);
-		double d = (double)customValues.get("size");
-		GL11.glScaled(d, d, d);	
-		RenderHelper.renderModel(this.model);		
+		double d = (double) customValues.get("size");
+		GL11.glScaled(d, d, d);
+		RenderHelper.renderModel(this.model);
 	}
-
 }

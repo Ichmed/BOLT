bf5ddcbb937c17ee9cc950c6c4f349f9746e7e87
Ichmed <mijomeyer@t-online.de>
2013-07-31 23:02:26 +0200

    created logic for logic_timer

---
+++ BOLT/src/entity/logic/Timer.java
@@ -0,0 +1,48 @@
+package entity.logic;
+
+import entity.Entity;
+
+public class Timer extends Entity
+{
+	private boolean isActive = false;
+	private long startingTime = 0;
+
+	@Override
+	public void initEntity()
+	{
+		if ((boolean) this.customValues.get("startActive"))
+			activate();
+	}
+
+	@Override
+	public void onTick()
+	{
+		if (this.isActive && System.currentTimeMillis() - this.startingTime < (int) this.customValues.get("durration"))
+		{
+			this.triggerEvent("onTimerFinished");
+			if ((boolean) this.customValues.get("reset"))
+				this.startingTime = System.currentTimeMillis();
+			else this.isActive = false;
+		}
+	}
+
+	public void activate()
+	{
+		this.startingTime = System.currentTimeMillis();
+		this.isActive = true;
+		this.triggerEvent("onActivation");
+	}
+
+	public void reset(boolean deactivate)
+	{
+		this.startingTime = System.currentTimeMillis();
+		if (deactivate)
+			this.isActive = false;
+		this.triggerEvent("onReset");
+	}
+
+	public void setDuration(int duration)
+	{
+		this.customValues.put("duration", duration);
+	}
+}

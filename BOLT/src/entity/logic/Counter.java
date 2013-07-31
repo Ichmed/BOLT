1195628dc2cf79c7d4981e4438a53ea520e8fa21
Ichmed <mijomeyer@t-online.de>
2013-07-31 18:46:48 +0200

    created logic for logic_counter

---
+++ BOLT/src/entity/logic/Counter.java
@@ -0,0 +1,39 @@
+package entity.logic;
+
+import entity.Entity;
+
+public class Counter extends Entity
+{
+	@Override
+	public void onTick()
+	{
+		if ((int) this.customValues.get("value") >= (int) this.customValues.get("targetValue"))
+			this.triggerEvent("onReachTargetValue");
+	}
+
+	@Override
+	public void initEntity()
+	{
+		this.customValues.put("value", (int) this.customValues.get("startingValue"));
+	}
+
+	public void increment()
+	{
+		int i = (int) this.customValues.get("value") + (int) this.customValues.get("incrementValue");
+		this.customValues.put("value", i);
+		this.triggerEvent("onIncrement");
+	}
+
+	public void decrement()
+	{
+		int i = (int) this.customValues.get("value") - (int) this.customValues.get("decrementValue");
+		this.customValues.put("value", i);
+		this.triggerEvent("onDecrement");
+	}
+
+	public void reset()
+	{
+		this.customValues.put("value", (int) this.customValues.get("startingValue"));
+		this.triggerEvent("onReset");
+	}
+}
